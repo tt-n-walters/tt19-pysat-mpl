07652cdf03519ba94ef12cc60657a7581a849e3d
nw <nico.walters@techtalents.es>
2020-05-23 18:15:45 +0200

    First 3d plot

--- plotting3d.py
+++ plotting3d.py
@@ -4,10 +4,19 @@ from mpl_toolkits import mplot3d
 from ranges import stepped
 import math
 
-x_coords = stepped(0, 2 * math.pi, 0.05)
 
 def y(x):
     return math.sin(x)
 
 
+
+figure = plt.figure()
+axes = mplot3d.Axes3D(figure)
+
+x_coords = stepped(0, 2 * math.pi, 0.05)
+y_coords = [y(x) for x in x_coords]
+
+axes.plot(x_coords, y_coords)
+
+plt.show()
 
