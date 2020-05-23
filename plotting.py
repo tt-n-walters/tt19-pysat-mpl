bc1cd6dd6f98da09d96f73e7e1430eaabd228a05
nw <nico.walters@techtalents.es>
2020-05-23 17:21:25 +0200

    Initial commit

---
+++ plotting.py
@@ -0,0 +1,21 @@
+import matplotlib.pyplot as plt
+import math
+
+from ranges import stepped
+
+
+def y(x):
+    return math.sin(x)
+
+
+x_coords = stepped(-10, 10, 0.1)
+y_coords = [y(x) for x in x_coords]
+
+y_coords = []
+for x in x_coords:
+    y_coords.append(y(x))
+
+
+plt.plot(x_coords, y_coords)
+plt.grid()
+plt.show()
